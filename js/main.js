536ea7cdbb690914de42947094d2cf559d1040cd
tsaitsai <vivian41521@yahoo.com.tw>
2021-02-23 22:32:54 +0800

    Fixed main.js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,21 @@
 $(document).ready(function () {
-
   $(document).scroll(function (e) {
     if ($(this).scrollTop() > 10) {
-      $('.nav').addClass('scrolled');
+      $(".nav").addClass("scrolled");
+      $(".nav-link").addClass("scrolled");
+      $(".navbar img").attr(
+        "src",
+        "http://droitthemes.com/html/saasland/img/logo.png"
+      );
     }
-    else {
-      $('.nav').removeClass('scrolled');
+
+    if ($(this).scrollTop() < 10) {
+      $(".nav").removeClass("scrolled");
+      $(".nav-link").removeClass("scrolled");
+      $(".navbar img").attr(
+        "src",
+        "http://droitthemes.com/html/saasland/img/logo2.png"
+      );
     }
   });
-
-  $(function () {
-    $(document).scroll(function () {
-      if ($(this).scrollTop() > 10) {
-        $('.navbar img').attr('src', 'http://droitthemes.com/html/saasland/img/logo.png');
-      }
-      if ($(this).scrollTop() < 10) {
-        $('.navbar img').attr('src', 'http://droitthemes.com/html/saasland/img/logo2.png');
-      }
-    })
-  });
-
 });
-
-
